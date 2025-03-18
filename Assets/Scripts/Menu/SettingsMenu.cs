02734bb560798984ba13f71ac0dbb8508c464073
Jose Zafrilla Ruiz <jozaru27@gmail.com>
2025-03-18 02:33:35 +0100

    Trabajado en menús
    Logo añadido a menú principal, creado el menú de ajustes de audio y vídeo, con las opciones correspondientes (falta trabajar en la estética y en que las opciones modifiquen las configuraciones correspondientes)

---
+++ Assets/Scripts/Menu/SettingsMenu.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+
+    public GameObject panelAudio;
+    public GameObject panelVideo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowAudioSettings();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void ShowAudioSettings()
+    {
+        panelAudio.SetActive(true);
+        panelVideo.SetActive(false);
+    }
+
+
+    public void ShowVideoSettings()
+    {
+        panelAudio.SetActive(false);
+        panelVideo.SetActive(true);
+    }
+}
