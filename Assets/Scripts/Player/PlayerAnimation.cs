5edb519003eb005a9d0548097a8ef82020350ca3
FrancescDB <duartbaldovifrancesc@gmail.com>
2025-03-20 11:59:06 +0100

    Animaciones Mejorada

--- Assets/Scripts/Player/PlayerAnimation.cs
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -13,6 +13,7 @@ public class PlayerAnimation : MonoBehaviour
 
     public void Idle()
     {
+        Debug.Log("IDLE");
         playerAnim.SetBool("Idle", true);
         playerAnim.SetBool("Attack", false);
         playerAnim.SetBool("Run", false);
@@ -22,22 +23,27 @@ public class PlayerAnimation : MonoBehaviour
 
     public void Attack()
     {
+        Debug.Log("ATTACK");
         playerAnim.SetBool("Idle", false);
         playerAnim.SetBool("Attack", true);
         playerAnim.SetBool("Run", false);
         playerAnim.SetBool("Hit", false);
+        playerAnim.SetBool("Dash", false);
     }
 
     public void Hit()
     {
+        Debug.Log("HIT");
         playerAnim.SetBool("Idle", false);
         playerAnim.SetBool("Attack", false);
         playerAnim.SetBool("Run", false);
         playerAnim.SetBool("Hit", true);
+        playerAnim.SetBool("Dash", false);
     }
 
     public void Run()
     {
+        Debug.Log("RUN");
         playerAnim.SetBool("Idle", false);
         playerAnim.SetBool("Attack", false);
         playerAnim.SetBool("Run", true);
@@ -47,11 +53,13 @@ public class PlayerAnimation : MonoBehaviour
 
     public void Dash()
     {
+        Debug.Log("DASH");
         playerAnim.SetBool("Dash", true);
     }
 
     public void Death()
     {
+        Debug.Log("DEATH");
         playerAnim.SetBool("Death", true);
     }
 }
