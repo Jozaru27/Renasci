a0247b7ca0134745f917c5e43e1f4007815593e0
CarlosDominguez2002 <carlosdominguezpita@gmail.com>
2025-03-28 11:42:10 +0100

    animación reliquia en el suelo finiquitada

--- Assets/Scripts/PassiveRelics/RelicsGroundAnimation.cs
+++ Assets/Scripts/PassiveRelics/RelicsGroundAnimation.cs
@@ -7,35 +7,50 @@ public class RelicsGroundAnimation : MonoBehaviour
     GameObject relic; 
     float maxDistanceY = 3;
     float lowestDistanceY = -3;
-    float counter = 0;
+    float counter = 2;
+    float timePass = 0;
     bool itGot = false;
     Vector3 originalPos;
-    float finalPos;
+    //float finalPos;
+    Vector3 finalPos1;
+    float middlePoint;
+    public AnimationCurve curve;
     // Start is called before the first frame update
     void Start()
     {
         relic = this.gameObject;
         originalPos = relic.transform.position;
-        finalPos = originalPos.y + 1;
+        //finalPos = originalPos.y + 1;
+        finalPos1 = new Vector3(originalPos.x, originalPos.y + 1, originalPos.z);
+        middlePoint = originalPos.y / finalPos1.y;
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         //cambia la direccion de arriba a abajo segun el valor de itGot
         if (itGot == false)
         {
-            relic.transform.position += new Vector3(0, 0.002f, 0);
-            if(relic.transform.position.y >= finalPos)
+            timePass += Time.deltaTime;
+            //relic.transform.position += new Vector3(0, 0.002f, 0);
+            //float t = Mathf.PingPong(Time.time / counter, 1);
+            relic.transform.position = Vector3.Lerp(originalPos,finalPos1,curve.Evaluate(timePass/counter));
+            if(timePass >=counter)
             {
                 itGot = true;
+                timePass = 0;
             }
         }else if(itGot == true)
         {
-            relic.transform.position -= new Vector3(0, 0.002f, 0);
-            if(relic.transform.position == originalPos)
+            timePass += Time.deltaTime;
+            //relic.transform.position -= new Vector3(0, 0.002f, 0);
+            //float t = Mathf.PingPong(Time.time / counter, 1);
+            relic.transform.position = Vector3.Lerp(finalPos1,originalPos, curve.Evaluate(timePass / counter));
+            if (timePass >= counter)
             {
                 itGot = false;
+                timePass = 0;
             }
         }
 
