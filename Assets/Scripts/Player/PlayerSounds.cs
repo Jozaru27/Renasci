5453eabba3d320072591f30efaf85e2958ac8de3
CarlosDominguez2002 <carlosdominguezpita@gmail.com>
2025-04-04 09:39:02 +0200

    sonidos varios implementados
    -paso 1
    -paso 2
    -ataque
    -muerte
    -Dash

--- Assets/Scripts/Player/PlayerSounds.cs
+++ Assets/Scripts/Player/PlayerSounds.cs
@@ -8,6 +8,9 @@ public class PlayerSounds : MonoBehaviour
 
     public AudioClip step1;
     public AudioClip step2;
+    public AudioClip dash;
+    public AudioClip attack;
+    public AudioClip death;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +22,7 @@ public class PlayerSounds : MonoBehaviour
     {
         
     }
+    /*
     public void Step1Sound()
     {
         //audioSource.clip = step1;
@@ -29,5 +33,37 @@ public class PlayerSounds : MonoBehaviour
     {
         //audioSource.clip = step2;
         audioSource.PlayOneShot(step2);
+    }
+    */
+    
+    public void playStepSounds()
+    {
+        int rand = Random.Range(1, 3);
+        if (rand == 1)
+        {
+            audioSource.PlayOneShot(step1, 5f);
+        }
+        else
+        {
+            audioSource.PlayOneShot(step2, 5f);
+        }
+    }
+
+    public void playDashSound()
+    {
+        //audioSource.Stop();
+        audioSource.PlayOneShot(dash,5f);
+    }
+
+    public void playAttackSound()
+    {
+        //audioSource.Stop();
+        audioSource.PlayOneShot(attack, 5f);
+    }
+
+    public void playDeathSound()
+    {
+        audioSource.Stop();
+        audioSource.PlayOneShot(death, 5f);
     }
 }
