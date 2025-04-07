b5671ce6451b814ad700a0d46d0ca0cf4eded595
FrancescDB <duartbaldovifrancesc@gmail.com>
2025-04-07 12:33:19 +0200

    Trampa de pinchos funcional

--- Assets/Scripts/Traps/DamageTrap.cs
+++ Assets/Scripts/Traps/DamageTrap.cs
@@ -4,13 +4,6 @@ using UnityEngine;
 
 public class DamageTrap : MonoBehaviour
 {
-    Animator anim;
-
-    private void Start()
-    {
-        anim.GetComponent<Animator>();
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
