6ebeaa0248fe6b616dca3bde17a97f478ee64152
Jose Zafrilla Ruiz <jozaru27@gmail.com>
2025-04-08 13:09:13 +0200

    Small Fix
    Push Trap Script

--- Assets/Scripts/Traps/PushTrap.cs
+++ Assets/Scripts/Traps/PushTrap.cs
@@ -1,55 +1,54 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PushTrap : MonoBehaviour
 {
     public Animator animator;
     public GameObject pushZone;
-
     public float pushForce = 10f;
     public float delayBeforePush = 0.5f;
-    public float resetTime = 2f;
+    public float zoneActiveTime = 0.2f;
 
     private bool isPushing = false;
 
-    private void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision collision)
     {
-        if (!isPushing && other.CompareTag("Player"))
-        {
-            StartCoroutine(ActivateTrap(other));
-        }
+        if (isPushing) return;
+        if (!collision.gameObject.CompareTag("Player")) return;
+
+        Debug.Log("Player collided with DetectionZone");
+        StartCoroutine(ActivateTrap(collision.collider));
     }
 
     private IEnumerator ActivateTrap(Collider playerCollider)
     {
         isPushing = true;
 
-        animator.SetBool("push", true);
+        animator.Play("IdlePush", 0, 0f);
 
         yield return new WaitForSeconds(delayBeforePush);
 
         pushZone.SetActive(true);
+        Debug.Log("PushZone ON");
 
-        Rigidbody playerRb = playerCollider.GetComponent<Rigidbody>();
-        if (playerRb != null && IsPlayerInPushZone(playerCollider))
+        Rigidbody rb = playerCollider.GetComponent<Rigidbody>();
+        if (rb != null && IsInsidePushZone(playerCollider))
         {
-            Vector3 pushDir = (playerRb.transform.position - transform.position).normalized;
-            playerRb.AddForce(pushDir * pushForce, ForceMode.Impulse);
+            Vector3 dir = (playerCollider.transform.position - transform.position).normalized;
+            rb.AddForce(dir * pushForce, ForceMode.Impulse);
+            Debug.Log("Player pushed");
         }
 
-        yield return new WaitForSeconds(0.2f);
+        yield return new WaitForSeconds(zoneActiveTime);
         pushZone.SetActive(false);
+        Debug.Log("PushZone OFF");
 
-        yield return new WaitForSeconds(resetTime);
-
-        animator.SetBool("push", false);
         isPushing = false;
     }
 
-    private bool IsPlayerInPushZone(Collider playerCollider)
+    private bool IsInsidePushZone(Collider player)
     {
-        Collider pushCollider = pushZone.GetComponent<Collider>();
-        return pushCollider.bounds.Intersects(playerCollider.bounds);
+        Collider zone = pushZone.GetComponent<Collider>();
+        return zone.bounds.Intersects(player.bounds);
     }
 }
