ce805ff73c61e8a13212092400b957ed27886481
CarlosDominguez2002 <carlosdominguezpita@gmail.com>
2025-04-08 18:46:59 +0200

    cambiado lo de los sonidos de ambiente

--- Assets/Scripts/Audio/AmbientSoundManager.cs
+++ Assets/Scripts/Audio/AmbientSoundManager.cs
@@ -8,64 +8,29 @@ public class AmbientSoundManager : MonoBehaviour
 {
     AudioSource audioSource;
 
-    public string folder = "Assets/Sounds/AmbientSounds";
+   public List<AudioClip> ambientSoundsClips = new List<AudioClip>();
 
-    AudioClip[] ambientSoundsClips;
-    string[] ambientSoundsFiles;
-
-    int ambientSoundClip;
+    int ambientSound;
+    int length=3;
     // Start is called before the first frame update
     void Awake()
     {
         audioSource = this.gameObject.GetComponent<AudioSource>();
-        ambientSoundsFiles = Directory.GetFiles(folder,"*mp3"); //obtenemos las rutas de los archivos y las guardamos en un array
 
-        ambientSoundsClips = new AudioClip[ambientSoundsFiles.Length];
+        ambientSound=Random.Range(0,ambientSoundsClips.Count);
 
-        for (int i = 0; i < ambientSoundsFiles.Length; i++)
-        {
-            string archivePath = ambientSoundsFiles[i];
-            StartCoroutine(LoadAudioClip(archivePath,i));
-        }
-    }
-
-    void Start()
-    {
-        ambientSoundClip = Random.Range(0,ambientSoundsClips.Length);
-
-        Debug.Log(ambientSoundsClips[ambientSoundClip]);
-
-        audioSource.clip = ambientSoundsClips[ambientSoundClip];
+        audioSource.clip=ambientSoundsClips[ambientSound];
         audioSource.Play();
     }
 
-    // Update is called once per frame
-    void Update()
+    void update()
     {
-        if (!audioSource.isPlaying)
-        {
-            ambientSoundClip = Random.Range(0, ambientSoundsClips.Length);
+        if(!audioSource.isPlaying){
+            ambientSound=Random.Range(0,ambientSoundsClips.Count);
+
+            audioSource.clip=ambientSoundsClips[ambientSound];
+             audioSource.Play();
         }
     }
-
-    System.Collections.IEnumerator LoadAudioClip(string archivePath,int index)
-    {
-        string filePath = "file:///" + archivePath.Replace("\\", "/");
-
-        UnityWebRequest request = UnityWebRequestMultimedia.GetAudioClip(filePath,AudioType.MPEG); //se le hace un apeticion local para obtener el clip que se encuentra en la ruta
-        yield return request.SendWebRequest();
-        if(request.result == UnityWebRequest.Result.Success)
-        {
-            ambientSoundsClips[index] = DownloadHandlerAudioClip.GetContent(request); //si obtiene el audioclip con exito, lo guarda en  array
-        }
-        else
-        {
-            Debug.Log("ERROR: AMBIENT SOUND COULDNT BE LOADED");
-        }
-    }
-        //AudioType audioType = AudioType.MPEG; 
-        //if (archivePath.EndsWith(".wav"))
-        //{
-            //audioType = AudioType.WAV; 
-        //}
+   
 }
