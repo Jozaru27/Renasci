611bfca0b1c318f1b44dac4b17741e399580cd47
Jose Zafrilla Ruiz <jozaru27@gmail.com>
2025-04-08 09:10:32 +0200

    Push Wall Trap
    Añadido Modelo actualizado
    
    Creada animación
    
    Creado script

--- Assets/Scripts/Traps/PushTrap.cs
+++ Assets/Scripts/Traps/PushTrap.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class PushTrap : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Animator animator;
+    public GameObject pushZone;
+
+    public float pushForce = 10f;
+    public float delayBeforePush = 0.5f;
+    public float resetTime = 2f;
+
+    private bool isPushing = false;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (!isPushing && other.CompareTag("Player"))
+        {
+            StartCoroutine(ActivateTrap(other));
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator ActivateTrap(Collider playerCollider)
     {
-        
+        isPushing = true;
+
+        animator.SetBool("push", true);
+
+        yield return new WaitForSeconds(delayBeforePush);
+
+        pushZone.SetActive(true);
+
+        Rigidbody playerRb = playerCollider.GetComponent<Rigidbody>();
+        if (playerRb != null && IsPlayerInPushZone(playerCollider))
+        {
+            Vector3 pushDir = (playerRb.transform.position - transform.position).normalized;
+            playerRb.AddForce(pushDir * pushForce, ForceMode.Impulse);
+        }
+
+        yield return new WaitForSeconds(0.2f);
+        pushZone.SetActive(false);
+
+        yield return new WaitForSeconds(resetTime);
+
+        animator.SetBool("push", false);
+        isPushing = false;
+    }
+
+    private bool IsPlayerInPushZone(Collider playerCollider)
+    {
+        Collider pushCollider = pushZone.GetComponent<Collider>();
+        return pushCollider.bounds.Intersects(playerCollider.bounds);
     }
 }
