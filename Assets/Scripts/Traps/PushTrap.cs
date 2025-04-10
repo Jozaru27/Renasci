c8f3d6e2aba9ef501781af056c660ead6386a078
FrancescDB <duartbaldovifrancesc@gmail.com>
2025-04-10 10:43:39 +0200

    Trampa de empuje a medias

--- Assets/Scripts/Traps/PushTrap.cs
+++ Assets/Scripts/Traps/PushTrap.cs
@@ -3,57 +3,91 @@ using UnityEngine;
 
 public class PushTrap : MonoBehaviour
 {
-    public Animator animator;
+    Animator animator;
+    GameObject playerObj;
+
+    [SerializeField] Collider trigger;
+
     public GameObject pushZone;
-    public float pushForce = 10f;
+    public float pushForce = 25f;
     public float delayBeforePush = 0.5f;
     public float zoneActiveTime = 0.2f;
 
     private bool isPushing = false;
+    bool playerInside;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
+        playerObj = GameObject.Find("Player");
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (isPushing) return;
-        if (!other.gameObject.CompareTag("Player")) return;
+        Debug.Log("A");
 
-        //Debug.Log("Player collided with DetectionZone");
-        StartCoroutine(ActivateTrap(other.GetComponent<Collider>()));
-    }
-
-    private IEnumerator ActivateTrap(Collider playerCollider)
-    {
-        isPushing = true;
-
-        animator.Play("IdlePush", 0, 0f);
-
-        yield return new WaitForSeconds(delayBeforePush);
-
-        pushZone.SetActive(true);
-        //Debug.Log("PushZone ON");
-
-        Rigidbody rb = playerCollider.GetComponent<Rigidbody>();
-        if (rb != null && IsInsidePushZone(playerCollider))
+        if (other.gameObject.CompareTag("Player") && !isPushing)
         {
-            Vector3 dir = (playerCollider.transform.position - transform.position).normalized;
-            rb.AddForce(dir * pushForce, ForceMode.Impulse);
-            //Debug.Log("Player pushed");
+            isPushing = true;
+            playerInside = true;
+            animator.Play("IdlePush");
         }
 
-        yield return new WaitForSeconds(zoneActiveTime);
-        pushZone.SetActive(false);
-        //Debug.Log("PushZone OFF");
+        //Debug.Log("Player collided with DetectionZone");
 
+
+        //StartCoroutine(ActivateTrap(other.GetComponent<Collider>()));
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+            playerInside = false;
+    }
+
+    public void PushPlayer()
+    {
+        if (playerInside)
+        {
+            Vector3 dir = (playerObj.transform.position - transform.position).normalized;
+            playerObj.gameObject.GetComponent<Rigidbody>().AddForce(dir * pushForce, ForceMode.Impulse);
+        }
+    }
+
+    public void PushingFalse()
+    {
         isPushing = false;
     }
 
-    private bool IsInsidePushZone(Collider player)
-    {
-        Collider zone = pushZone.GetComponent<Collider>();
-        return zone.bounds.Intersects(player.bounds);
-    }
+    //private IEnumerator ActivateTrap(Collider playerCollider)
+    //{
+    //    isPushing = true;
+
+    //    animator.Play("IdlePush", 0, 0f);
+
+    //    yield return new WaitForSeconds(delayBeforePush);
+
+    //    pushZone.SetActive(true);
+    //    //Debug.Log("PushZone ON");
+
+    //    Rigidbody rb = playerCollider.GetComponent<Rigidbody>();
+    //    if (rb != null && IsInsidePushZone(playerCollider))
+    //    {
+    //        Vector3 dir = (playerCollider.transform.position - transform.position).normalized;
+    //        rb.AddForce(dir * pushForce, ForceMode.Impulse);
+    //        //Debug.Log("Player pushed");
+    //    }
+
+    //    yield return new WaitForSeconds(zoneActiveTime);
+    //    pushZone.SetActive(false);
+    //    //Debug.Log("PushZone OFF");
+
+    //    isPushing = false;
+    //}
+
+    //private bool IsInsidePushZone(Collider player)
+    //{
+    //    Collider zone = pushZone.GetComponent<Collider>();
+    //    return zone.bounds.Intersects(player.bounds);
+    //}
 }
