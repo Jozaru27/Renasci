ca6a71726eab7894b7f5a3c72d83699f19931600
CarlosDominguez2002 <carlosdominguezpita@gmail.com>
2025-04-10 08:55:56 +0200

    cambios efectos de sonido
    -ahora no se repiten

--- Assets/Scripts/Audio/EffectSoundManager.cs
+++ Assets/Scripts/Audio/EffectSoundManager.cs
@@ -13,32 +13,70 @@ public class EffectSoundManager : MonoBehaviour
     int effectSound;
     int length=1;
     int chance1;
-    int chance2;
+
+    int prevEffectSound;
+    bool onceAct = false;
     // Start is called before the first frame update
     void Awake()
     {
         audioSource = this.gameObject.GetComponent<AudioSource>();
 
         effectSound=Random.Range(0,effectSoundsClips.Count);
-        
+        prevEffectSound = effectSound;
 
+        audioSource.clip = effectSoundsClips[effectSound];
+        audioSource.Play();
+
+    }
+
+    private void Start()
+    {
+        
     }
 
     void Update()
     {
-       
-        if (!audioSource.isPlaying)
+        chance1 = Random.Range(1, 101);
+
+        if (!audioSource.isPlaying && onceAct == false)
         {
-            effectSound = Random.Range(0, effectSoundsClips.Count);
-            chance1 = Random.Range(1, 101);
-            
-            if (chance1 == 5)
-            {
-                //Debug.Log("susurro susurrez");
-                audioSource.clip = effectSoundsClips[effectSound];
-                audioSource.Play();
-            }
+            Debug.Log("Hola");
+            StartCoroutine(playEffectSound());
+
+            onceAct = true;
         }
+        /*
+         if (!audioSource.isPlaying)
+         {
+             effectSound = Random.Range(0, effectSoundsClips.Count);
+             chance1 = Random.Range(1, 101);
+
+             if (chance1 == 5)
+             {
+                 //Debug.Log("susurro susurrez");
+                 audioSource.clip = effectSoundsClips[effectSound];
+                 audioSource.Play();
+             }
+         }
+        */
+    }
+
+    IEnumerator playEffectSound()
+    {
+        Debug.Log("corrutina corrutierrez");
+        while (effectSound == prevEffectSound && chance1!=1)
+        {
+            chance1 = Random.Range(1, 101);
+            effectSound = Random.Range(0, effectSoundsClips.Count);
+        }
+        prevEffectSound = effectSound;
+
+        audioSource.clip = effectSoundsClips[effectSound];
+        audioSource.Play();
+
+        yield return new WaitUntil(() => !audioSource.isPlaying);
+        yield return new WaitForSeconds(15f);
+        StartCoroutine(playEffectSound());
     }
    
 }
